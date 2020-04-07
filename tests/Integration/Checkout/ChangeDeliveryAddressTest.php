80ba8fba6083da504154ada24a963696f0563f5a
Farzam Tahmasebmirza <farzam.tahmasebmirza@oxid-esales.com>
2020-04-07 15:00:07 +0200

    OXDEV-3173 Migrate BasketFrontendTest::testFrontendVAT() test

--- tests/Integration/Checkout/ChangeDeliveryAddressTest.php
+++ tests/Integration/Checkout/ChangeDeliveryAddressTest.php
@@ -4,26 +4,27 @@
  */
 namespace OxidEsales\EshopCommunity\Tests\Integration\Checkout;
 
-use oxField;
-use OxidEsales\EshopCommunity\Core\ShopIdCalculator;
-use oxRegistry;
+use OxidEsales\Eshop\Application\Model\Basket;
+use OxidEsales\Eshop\Application\Model\VatSelector;
+use OxidEsales\Eshop\Core\Registry;
+use OxidEsales\Eshop\Application\Model\User;
+use OxidEsales\Eshop\Core\Field;
+use OxidEsales\Eshop\Core\ShopIdCalculator;
 
 class ChangeDeliveryAddressTest extends \OxidTestCase
 {
-    const TEST_ARTICLE_ID = '1951';
+    private const TEST_ARTICLE_ID = '1951';
+    private const GERMANY_COUNTRY_ID = 'a7c40f631fc920687.20179984';
+    private const AUSTRIA_COUNTRY_ID = 'a7c40f6320aeb2ec2.72885259';
+    private const SWITZERLAND_COUNTRY_ID = 'a7c40f6321c6f6109.43859248';
+    private const BELGIUM_COUNTRY_ID = 'a7c40f632e04633c9.47194042';
 
-    /**
-     * Fixture setUp.
-     */
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
     }
 
-    /**
-     * Fixture tearDown.
-     */
-    protected function tearDown()
+    protected function tearDown(): void
     {
         $this->cleanUpTable('oxuser');
         $this->cleanUpTable('oxuserbaskets');
@@ -32,47 +33,68 @@ class ChangeDeliveryAddressTest extends \OxidTestCase
         parent::tearDown();
     }
 
+    public function testVatForBelgiumCountry(): void
+    {
+        $user = $this->createActiveBelgiumUser();
+
+        // Vat will be False if country is EU and has not a valid VatID
+        $vatSelector = Registry::get(VatSelector::class);
+        $this->assertSame(0, (int)$vatSelector->getUserVat($user));
+        $this->assertSame(0, (int)$vatSelector->getUserVat($user, true)); //no cache
+    }
+
     /**
      * Verify that the oxVatSelector respects user country changes.
      */
-    public function testVatSelectorOnActiveUserCountryChange()
+    public function testVatSelectorOnActiveUserCountryChange(): void
     {
         //create active user
-        $user = $this->createActiveUser(); //Switzerland user
+        $user = $this->createSwitzerlandActiveUser(); //Switzerland user
 
         //assert zero VAT for Switzerland
-        $vatSelector = oxRegistry::get('oxVatSelector');
+        $vatSelector = Registry::get(VatSelector::class);
         $this->assertSame(0, $vatSelector->getUserVat($user));
         $this->assertSame(0, $vatSelector->getUserVat($user, true)); //no cache
 
         //Change to german address
-        $this->changeUser();
+        $this->changeUserAddressToGermany();
 
         //verify that the active user was updated
-        $user = oxNew('oxUser');
+        $user = oxNew(User::class);
         $user->loadActiveUser();
-        $this->assertSame('Hahnentritt', $user->oxuser__oxlname->value);
-        $this->assertSame('a7c40f631fc920687.20179984', $user->oxuser__oxcountryid->value);
+        $this->assertSame(self::GERMANY_COUNTRY_ID, $user->oxuser__oxcountryid->value);
 
         //verify german VAT
-        $vatSelector = oxRegistry::get('oxVatSelector');
+        $vatSelector = Registry::get('oxVatSelector');
         $this->assertFalse($vatSelector->getUserVat($user, true));
         $this->assertFalse($vatSelector->getUserVat($user));
+
+        $this->changeUserAddressToAustria();
+
+        //verify that the active user was updated
+        $user = oxNew(User::class);
+        $user->loadActiveUser();
+        $this->assertSame(self::AUSTRIA_COUNTRY_ID, $user->oxuser__oxcountryid->value);
+
+        //verify Austria VAT
+        $vatSelector = Registry::get(VatSelector::class);
+        $this->assertSame(0, (int)$vatSelector->getUserVat($user));
+        $this->assertSame(0, (int)$vatSelector->getUserVat($user, true)); //no cache
     }
 
     /**
      * Test basket calculation when user country changes during checkout.
      * Test case when we explicitly set user via oxBasket::setBasketUser.
      */
-    public function testBasketCalculationOnUserCountryChangeExplicitlySetBasketUser()
+    public function testBasketCalculationOnUserCountryChangeExplicitlySetBasketUser(): void
     {
         //no user logged in atm, create a basket
-        $basket = oxNew('oxBasket');
+        $basket = oxNew(Basket::class);
         $basket->addToBasket(self::TEST_ARTICLE_ID, 1); //14 EUR
         $this->getSession()->setBasket($basket);
 
         //create user, as soon at it is set in session, it is available for basket as well
-        $user = $this->createActiveUser(); //Switzerland user
+        $user = $this->createSwitzerlandActiveUser(); //Switzerland user
         $basket->setBasketUser($user);
 
         //verify basket calculation results
@@ -81,7 +103,7 @@ class ChangeDeliveryAddressTest extends \OxidTestCase
         $this->assertSame(11.76, $basket->getBruttoSum()); //no VAT for Switzerland
 
         //Change to german address
-        $this->changeUser();
+        $this->changeUserAddressToGermany();
 
         //verify that the basket user is up to date
         $basket = $this->getSession()->getBasket();
@@ -97,15 +119,15 @@ class ChangeDeliveryAddressTest extends \OxidTestCase
     /**
      * Test basket calculation when user country changes during checkout.
      */
-    public function testBasketCalculationOnUserCountryChange()
+    public function testBasketCalculationOnUserCountryChange(): void
     {
         //no user logged in atm, create a basket
-        $basket = oxNew('oxBasket');
+        $basket = oxNew(Basket::class);
         $basket->addToBasket(self::TEST_ARTICLE_ID, 1); //14 EUR
         $this->getSession()->setBasket($basket);
 
         //create user, as soon at it is set in session, it is available for basket as well
-        $this->createActiveUser(); //Switzerland user
+        $this->createSwitzerlandActiveUser(); //Switzerland user
 
         //verify basket calculation results
         $basket->calculateBasket(true);
@@ -113,7 +135,7 @@ class ChangeDeliveryAddressTest extends \OxidTestCase
         $this->assertSame(11.76, $basket->getBruttoSum()); //no VAT for Switzerland
 
         //Change to german address
-        $this->changeUser();
+        $this->changeUserAddressToGermany();
 
         //verify that the basket user is up to date
         $basket = $this->getSession()->getBasket();
@@ -126,38 +148,39 @@ class ChangeDeliveryAddressTest extends \OxidTestCase
 
     /**
      * Insert test user, set to session
+     *
+     * @return User
      */
-    private function createActiveUser()
+    private function createSwitzerlandActiveUser(): User
     {
-        $sTestUserId = substr_replace(oxRegistry::getUtilsObject()->generateUId(), '_', 0, 1);
+        $sTestUserId = substr_replace(Registry::getUtilsObject()->generateUId(), '_', 0, 1);
 
-        $user = oxNew('oxUser');
+        $user = oxNew(User::class);
         $user->setId($sTestUserId);
 
-        $user->oxuser__oxactive = new oxField('1', oxField::T_RAW);
-        $user->oxuser__oxrights = new oxField('user', oxField::T_RAW);
-        $user->oxuser__oxshopid = new oxField(ShopIdCalculator::BASE_SHOP_ID, oxField::T_RAW);
-        $user->oxuser__oxusername = new oxField('testuser@oxideshop.dev', oxField::T_RAW);
-        $user->oxuser__oxpassword = new oxField(
+        $user->oxuser__oxactive = new Field('1');
+        $user->oxuser__oxrights = new Field('user');
+        $user->oxuser__oxshopid = new Field(ShopIdCalculator::BASE_SHOP_ID);
+        $user->oxuser__oxusername = new Field('testuser@oxideshop.dev');
+        $user->oxuser__oxpassword = new Field(
             'c630e7f6dd47f9ad60ece4492468149bfed3da3429940181464baae99941d0ffa5562' .
-                                                'aaecd01eab71c4d886e5467c5fc4dd24a45819e125501f030f61b624d7d',
-            oxField::T_RAW
+                                                'aaecd01eab71c4d886e5467c5fc4dd24a45819e125501f030f61b624d7d'
         ); //password is asdfasdf
-        $user->oxuser__oxpasssalt = new oxField('3ddda7c412dbd57325210968cd31ba86', oxField::T_RAW);
-        $user->oxuser__oxcustnr = new oxField('667', oxField::T_RAW);
-        $user->oxuser__oxfname = new oxField('Erna', oxField::T_RAW);
-        $user->oxuser__oxlname = new oxField('Helvetia', oxField::T_RAW);
-        $user->oxuser__oxstreet = new oxField('Dorfstrasse', oxField::T_RAW);
-        $user->oxuser__oxstreetnr = new oxField('117', oxField::T_RAW);
-        $user->oxuser__oxcity = new oxField('Oberbuchsiten', oxField::T_RAW);
-        $user->oxuser__oxcountryid = new oxField('a7c40f6321c6f6109.43859248', oxField::T_RAW);
-        $user->oxuser__oxzip = new oxField('4625', oxField::T_RAW);
-        $user->oxuser__oxsal = new oxField('MRS', oxField::T_RAW);
-        $user->oxuser__oxactive = new oxField('1', oxField::T_RAW);
-        $user->oxuser__oxboni = new oxField('1000', oxField::T_RAW);
-        $user->oxuser__oxcreate = new oxField('2015-05-20 22:10:51', oxField::T_RAW);
-        $user->oxuser__oxregister = new oxField('2015-05-20 22:10:51', oxField::T_RAW);
-        $user->oxuser__oxboni = new oxField('1000', oxField::T_RAW);
+        $user->oxuser__oxpasssalt = new Field('3ddda7c412dbd57325210968cd31ba86');
+        $user->oxuser__oxcustnr = new Field('667');
+        $user->oxuser__oxfname = new Field('Erna');
+        $user->oxuser__oxlname = new Field('Helvetia');
+        $user->oxuser__oxstreet = new Field('Dorfstrasse');
+        $user->oxuser__oxstreetnr = new Field('117');
+        $user->oxuser__oxcity = new Field('Oberbuchsiten');
+        $user->oxuser__oxcountryid = new Field(self::SWITZERLAND_COUNTRY_ID);
+        $user->oxuser__oxzip = new Field('4625');
+        $user->oxuser__oxsal = new Field('MRS');
+        $user->oxuser__oxactive = new Field('1');
+        $user->oxuser__oxboni = new Field('1000');
+        $user->oxuser__oxcreate = new Field('2015-05-20 22:10:51');
+        $user->oxuser__oxregister = new Field('2015-05-20 22:10:51');
+        $user->oxuser__oxboni = new Field('1000');
 
         $user->save();
 
@@ -167,20 +190,74 @@ class ChangeDeliveryAddressTest extends \OxidTestCase
     }
 
     /**
-     * Test helper, change user to german address.
+     * @return User
      */
-    private function changeUser()
+    private function createActiveBelgiumUser(): User
     {
-        //now change the user address
-        $rawValues = array('oxuser__oxfname'     => 'Erna',
-                           'oxuser__oxlname'     => 'Hahnentritt',
-                           'oxuser__oxstreetnr'  => '117',
-                           'oxuser__oxstreet'    => 'Landstrasse',
-                           'oxuser__oxzip'       => '22769',
-                           'oxuser__oxcity'      => 'Hamburg',
-                           'oxuser__oxcountryid' => 'a7c40f631fc920687.20179984');
+        $sTestUserId = substr_replace(Registry::getUtilsObject()->generateUId(), '_', 0, 1);
 
-        $this->setRequestParameter('invadr', $rawValues);
+        $user = oxNew(User::class);
+        $user->setId($sTestUserId);
+
+        $user->oxuser__oxactive = new Field('1');
+        $user->oxuser__oxrights = new Field('user');
+        $user->oxuser__oxshopid = new Field(ShopIdCalculator::BASE_SHOP_ID);
+        $user->oxuser__oxusername = new Field('testuser@oxideshop.dev');
+        $user->oxuser__oxpassword = new Field(
+            'c630e7f6dd47f9ad60ece4492468149bfed3da3429940181464baae99941d0ffa5562' .
+            'aaecd01eab71c4d886e5467c5fc4dd24a45819e125501f030f61b624d7d'
+        ); //password is asdfasdf
+        $user->oxuser__oxpasssalt = new Field('3ddda7c412dbd57325210968cd31ba86');
+        $user->oxuser__oxcustnr = new Field('667');
+        $user->oxuser__oxfname = new Field('Erna');
+        $user->oxuser__oxlname = new Field('Helvetia');
+        $user->oxuser__oxstreet = new Field('Dorfstrasse');
+        $user->oxuser__oxstreetnr = new Field('117');
+        $user->oxuser__oxcity = new Field('Oberbuchsiten');
+        $user->oxuser__oxcountryid = new Field(self::BELGIUM_COUNTRY_ID);
+        $user->oxuser__oxzip = new Field('4625');
+        $user->oxuser__oxsal = new Field('MRS');
+        $user->oxuser__oxactive = new Field('1');
+        $user->oxuser__oxcreate = new Field('2015-05-20 22:10:51');
+        $user->oxuser__oxregister = new Field('2015-05-20 22:10:51');
+        $user->oxuser__oxboni = new Field('1000');
+        $user->oxuser__oxustid = new  Field("BE0410521222");
+
+        $user->save();
+
+        $this->getSession()->setVariable('usr', $user->getId());
+
+        return $user;
+    }
+
+    private function changeUserAddressToGermany(): void
+    {
+        $this->setRequestParameter('invadr', [
+            'oxuser__oxfname'     => 'Erna',
+            'oxuser__oxlname'     => 'Hahnentritt',
+            'oxuser__oxstreetnr'  => '117',
+            'oxuser__oxstreet'    => 'Landstrasse',
+            'oxuser__oxzip'       => '22769',
+            'oxuser__oxcity'      => 'Hamburg',
+            'oxuser__oxcountryid' => self::GERMANY_COUNTRY_ID
+        ]);
+        $this->setRequestParameter('stoken', $this->getSession()->getSessionChallengeToken());
+
+        $userComponent = oxNew('oxcmp_user');
+        $this->assertSame('payment', $userComponent->changeUser());
+    }
+
+    private function changeUserAddressToAustria(): void
+    {
+        $this->setRequestParameter('invadr', [
+            'oxuser__oxfname'     => 'Erna',
+            'oxuser__oxlname'     => 'Hahnentritt',
+            'oxuser__oxstreetnr'  => '117',
+            'oxuser__oxstreet'    => 'Landstrasse',
+            'oxuser__oxzip'       => '3741',
+            'oxuser__oxcity'      => 'PULKAU',
+            'oxuser__oxcountryid' => self::AUSTRIA_COUNTRY_ID
+        ]);
         $this->setRequestParameter('stoken', $this->getSession()->getSessionChallengeToken());
 
         $userComponent = oxNew('oxcmp_user');
